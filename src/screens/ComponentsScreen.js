ac2c00544c2ca5e1adae9c16635c2276de314603
mikeygbd <mikey.soares@gmail.com>
2019-09-20 10:33:23 -0400

    created a const variable greeting and inserted it into the view

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
+  const greeting = <Text> Hello to You!</Text>
   return (
       <View>
       <Text style={styles.textStyle}>I'm the ComponentsScreen component</Text>
+      {greeting}
       </View>
   )
 }
