7c4f53850be65cdc9abb36052d0668dbc38728c4
mikeygbd <mikey.soares@gmail.com>
2019-09-20 10:43:25 -0400

    corrected

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-  const title = <Text style={{fontSize: 45}}> Getting started with React Native!</Text>
-  const name = <Text style={{fontSize: 20}}> My name is Michael Soares</Text>
+  
+  const name = "Michael Soares"
 
   return (
       <View>
-      {title}
-      {name}
+      <Text style={{fontSize: 45}}> Getting started with React Native!</Text>
+      <Text style={{fontSize: 20}}> My name is {name}</Text>
       </View>
   )
 }
