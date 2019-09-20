f0e2c014565daa4f0e615c43b09851499a885fff
mikeygbd <mikey.soares@gmail.com>
2019-09-20 10:46:10 -0400

    changed method of using styles

--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const ComponentsScreen = () => {
-  
+
   const name = "Michael Soares"
 
   return (
       <View>
-      <Text style={{fontSize: 45}}> Getting started with React Native!</Text>
-      <Text style={{fontSize: 20}}> My name is {name}</Text>
+      <Text style={styles.titleStyle}> Getting started with React Native!</Text>
+      <Text style={styles.subHeaderStyle}> My name is {name}</Text>
       </View>
   )
 }
 const styles = StyleSheet.create({
-  textStyle: {
-    fontSize: 40,
+  titleStyle: {
+    fontSize: 45,
+  },
+  subHeaderStyle: {
+    fontSize: 20,
   },
 });
 
