c1ffa476e670ed7e2ca981e21fd94b07a8992aea
mikeygbd <mikey.soares@gmail.com>
2019-09-21 15:16:57 -0400

    added button and touchable opacity to home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>BLAH BLAH!</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Welcome!</Text>
+      <Button
+        title="Go to Components Demo"
+        onPress={() => console.log('Button Pressed')}
+        />
+      <TouchableOpacity onPress={() => console.log('list pressed')}>
+        <Text>Go To List Demo</Text>
+      </TouchableOpacity>
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
