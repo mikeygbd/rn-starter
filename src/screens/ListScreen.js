4d247768800f618834e2bbc6172fcaa70ce01ece
mikeygbd <mikey.soares@gmail.com>
2019-09-21 15:03:51 -0400

    added age to friend and show friends agae

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -8,24 +8,24 @@ import {
 
 const ListScreen = () => {
 const friends = [
-  {name: 'Friend #1'},
-  {name: 'Friend #2'},
-  {name: 'Friend #3'},
-  {name: 'Friend #4'},
-  {name: 'Friend #5'},
-  {name: 'Friend #6'},
-  {name: 'Friend #7'},
-  {name: 'Friend #8'},
-  {name: 'Friend #9'},
+  {name: 'Friend #1', age: 20},
+  {name: 'Friend #2', age: 45},
+  {name: 'Friend #3', age: 32},
+  {name: 'Friend #4', age: 27},
+  {name: 'Friend #5', age: 53},
+  {name: 'Friend #6', age: 30},
+  {name: 'Friend #7', age: 19},
+  {name: 'Friend #8', age: 18},
+  {name: 'Friend #9', age: 17},
 ]
     return (
       <FlatList
-      horizontal
-      showsHorizontalScrollIndicator={false}
       keyExtractor={(friend) => friend.name }
       data={friends}
       renderItem={({item}) => {
-        return <Text style={styles.textStyle}>{item.name}</Text>
+        return <Text style={styles.textStyle}>
+        {item.name} - Age {item.age}
+        </Text>
       }}/>
     );
   }
@@ -33,7 +33,8 @@ const friends = [
 
 const styles = StyleSheet.create({
   textStyle: {
-    marginVertical: 50,
+    marginVertical: 10,
+
   },
 });
 
