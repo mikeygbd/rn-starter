d2fd1399aada8b89a13e4d9ab66a4a5a81474bf4
mikeygbd <mikey.soares@gmail.com>
2019-09-21 14:52:30 -0400

    added a key extractor

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import {
+  FlatList,
   View,
   Text,
   StyleSheet,
 } from 'react-native';
 
 const ListScreen = () => {
-
+const friends = [
+  {name: 'Friend #1'},
+  {name: 'Friend #2'},
+  {name: 'Friend #3'},
+  {name: 'Friend #4'},
+  {name: 'Friend #5'},
+  {name: 'Friend #6'},
+  {name: 'Friend #7'},
+  {name: 'Friend #8'},
+  {name: 'Friend #9'},
+]
     return (
-      <View style={styles.container}>
-        <Text>I'm the ListScreen component</Text>
-      </View>
+      <FlatList
+      keyExtractor={(friend) => friend.name }
+      data={friends}
+      renderItem={({item}) => {
+        return <Text>{item.name}</Text>
+      }}/>
     );
   }
-}
+
 
 const styles = StyleSheet.create({
   container: {
