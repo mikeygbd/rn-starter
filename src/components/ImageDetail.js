a507d28e8852c9370ed71078d8072310788cd0d0
mikeygbd <mikey.soares@gmail.com>
2019-09-21 19:28:01 -0400

    created the image detail component using the title props being passed in

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,24 @@
+/* @flow */
+
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+} from 'react-native';
+
+const ImageDetail = (props) => {
+console.log(props)
+    return (
+      <View >
+        <Text>{props.title}</Text>
+      </View>
+    );
+  }
+
+
+const styles = StyleSheet.create({
+
+});
+
+export default ImageDetail
