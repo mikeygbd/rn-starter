383ab45912399d10e21adf97be2f819fe47344ba
mikeygbd <mikey.soares@gmail.com>
2019-09-22 11:25:11 -0400

    dispatched the reducer and added colors const

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -30,10 +30,10 @@ const reducer = (state, action) => {
 const SquareScreen = ({}) => {
 
 
-
+  // dispatch - run my reducer - pass in an action
   const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 })
-  console.log(state)
 
+  const { red, green, blue } = state;
 
 
 
@@ -41,18 +41,18 @@ const SquareScreen = ({}) => {
     <View style={styles.container}>
     <ColorCounter
     color="Red"
-    onIncrease={() => }
-    onDecrease={() => }
+    onIncrease={() => dispatch({colorToChange: 'red', amount: COLOR_INCREMENT})}
+    onDecrease={() => dispatch({colorToChange: 'red', amount: -1 * COLOR_INCREMENT})}
     />
     <ColorCounter
     color="Blue"
-    onIncrease={() => }
-    onDecrease={() => }
+    onIncrease={() => dispatch({colorToChange: 'blue', amount: COLOR_INCREMENT})}
+    onDecrease={() => dispatch({colorToChange: 'blue', amount: -1 * COLOR_INCREMENT})}
     />
     <ColorCounter
     color="Green"
-    onIncrease={() => }
-    onDecrease={() => }
+    onIncrease={() => dispatch({colorToChange: 'green', amount: COLOR_INCREMENT})}
+    onDecrease={() => dispatch({colorToChange: 'green', amount: -1 * COLOR_INCREMENT})}
     />
     <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
 
