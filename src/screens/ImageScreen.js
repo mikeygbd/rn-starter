1654bdbe74a2a3dd4a285b8a9e9e01673e3c5d85
mikeygbd <mikey.soares@gmail.com>
2019-09-21 20:03:38 -0400

    added image score and formated the code nicely

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -12,9 +12,21 @@ const ImageScreen = () => {
 
     return (
       <View>
-        <ImageDetail title="Forest" imageSource={require('../../assets/forest.jpg')} />
-        <ImageDetail title="Beach" imageSource={require('../../assets/beach.jpg')} />
-        <ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')}/>
+        <ImageDetail
+          title="Forest"
+          imageSource={require('../../assets/forest.jpg')}
+          imageScore="8"
+        />
+        <ImageDetail
+          title="Beach"
+          imageSource={require('../../assets/beach.jpg')}
+          imageScore="7"
+        />
+        <ImageDetail
+          title="Mountain"
+          imageSource={require('../../assets/mountain.jpg')}
+          imageScore="9"
+        />
       </View>
     );
   }
