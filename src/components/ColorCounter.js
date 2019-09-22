a25ae3e65204eeecb6361f38e15a8a2af041a9a2
mikeygbd <mikey.soares@gmail.com>
2019-09-21 21:45:10 -0400

    created child color counter with two buttons one for increase and one for decrease

---
+++ src/components/ColorCounter.js
@@ -0,0 +1,31 @@
+/* @flow weak */
+
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button
+} from 'react-native';
+
+const ColorCounter = ({ color, onIncrease, onDecrease }) => (
+  <View style={styles.container}>
+    <Text>{color}</Text>
+    <Button
+    onPress={() => onIncrease()}
+    title={`Increase ${color}`}
+    />
+    <Button
+    onPress={() => onDecrease()}
+    title={`Decrease ${color}`}
+     />
+  </View>
+);
+
+export default ColorCounter;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
