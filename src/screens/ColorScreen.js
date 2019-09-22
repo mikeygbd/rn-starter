e2039e642bf3bc68ba8c33432760efccb206d97d
mikeygbd <mikey.soares@gmail.com>
2019-09-21 21:10:30 -0400

    created a flat list of randomly generated colors

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,21 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
   StyleSheet,
-  Button
+  Button,
+  FlatList
 } from 'react-native';
 
-const ColorScreen = ({}) => (
-  <View style={styles.container}>
-    <Button title="Add A Color"/>
+const ColorScreen = () => {
+ const [colors, setColors] = useState([])
+
+return (
+  <View>
+    <Button
+    title="Add A Color"
+    onPress={() => {
+      setColors([...colors, randomRgb()])
+    }}
+    />
+
+    <FlatList
+    keyExtractor={item => item }
+    data={colors}
+    renderItem={({ item }) => {
+      return (
+      <View
+      style={{ height: 100, width: 100, backgroundColor: item }}
+      />
+    )
+    }} />
   </View>
-);
+  )
+};
+
+const randomRgb = () => {
+  const red = Math.floor(Math.random() * 256)
+  const green = Math.floor(Math.random() * 256)
+  const blue = Math.floor(Math.random() * 256)
+  return `rgb(${red}, ${green}, ${blue})`
+}
 
 export default ColorScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+
 });
