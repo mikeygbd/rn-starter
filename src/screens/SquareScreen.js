290eecea06fe1f10b6aabd099ca99989dc7489c9
mikeygbd <mikey.soares@gmail.com>
2019-09-21 22:09:44 -0400

    created set color function that way you dont go over or uner the max and min color spectrum

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -13,23 +13,38 @@ const SquareScreen = ({}) => {
   const [green, setGreen] = useState(0)
   const [blue, setBlue] = useState(0)
   const COLOR_INCREMENT = 20
+  const setColor = (color, change) => {
+    // color === 'red, 'green, 'blue'
+    // change === +15, -15
+    switch (color) {
+      case 'red':
+      red + change > 255 || red + change < 0 ? null : setRed(red + change)
+      return;
+      case 'green':
+      green + change > 255 || green + change < 0 ? null : setGreen(green + change)
+      return;
+      case 'blue':
+      blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
+      return;
+    }
+  }
 
   return (
     <View style={styles.container}>
     <ColorCounter
     color="Red"
-    onIncrease={() => setRed(red + COLOR_INCREMENT)}
-    onDecrease={() => setRed(red - COLOR_INCREMENT)}
+    onIncrease={() => setColor('red', COLOR_INCREMENT)}
+    onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
     />
     <ColorCounter
     color="Blue"
-    onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
-    onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+    onIncrease={() => setColor('blue', + COLOR_INCREMENT)}
+    onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}
     />
     <ColorCounter
     color="Green"
-    onIncrease={() => setGreen(green + COLOR_INCREMENT)}
-    onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+    onIncrease={() => setColor('green', + COLOR_INCREMENT)}
+    onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}
     />
     <View style={{ height: 150, width: 150, backgroundColor: `rgb(${red}, ${green}, ${blue})` }} />
 
