16096e2b4473eec689d936bd5a3929d2fd21242a
mikeygbd <mikey.soares@gmail.com>
2019-09-27 14:46:03 -0400

    changed name to password and added tearnary text element if the password is less than 5 characters

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -9,19 +9,20 @@ import {
 } from 'react-native';
 
 const TextScreen = () => {
-  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
 
   return (
   <View>
-  <Text>Enter Name:</Text>
+  <Text>Enter Password:</Text>
     <TextInput
     style={styles.input}
     autoCapitalize="none"
     autoCorrect={false}
-    value={name}
-    onChangeText={(newValue) => setName(newValue) }
+    value={password}
+    onChangeText={(newValue) => setPassword(newValue) }
     />
-    <Text>My Name Is {name}</Text>
+
+    {password.length < 5 ? <Text>Password must be longer than 5 characters</Text> : null}
   </View>
 );
 }
